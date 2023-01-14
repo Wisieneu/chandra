f22f2b69b141a2e1a111a4301c40d7ff0fe97e87
wisie <wisnia501@gmail.com>
2023-01-14 21:52:17 +0100

    add login, signup and home views and urls

--- chandra/urls.py
+++ chandra/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from main import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('/',),
-    path('login/', auth_views.LoginView.as_view(template_name=r'users\login.html'), name='login')
+    path('', home_views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('signup/', user_views.signup, name='signup')
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,6 @@ from django.contrib import auth
 
 def login(request):
     return render(request, 'users/login.html')
+
+def signup(request):
+    return render(request, 'users/signup.html')
