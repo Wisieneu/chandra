e7c4e68e2327580585bda2fe765f7148402d2239
wisie <wisnia501@gmail.com>
2023-01-15 03:09:52 +0100

    add user<->profile relation

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     display_name = models.CharField(max_length=32)
@@ -12,3 +13,5 @@ class Profile(models.Model):
     def __str__(self) -> str:
         if self.first_name and self.last_name:
             return f'{self.first_name} {self.last_name}'
+        else:
+            return self.display_name
