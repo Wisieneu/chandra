3368b34cb40e372723aa6a05cb7bfd24aa5eff37
wisie <wisnia501@gmail.com>
2023-01-15 01:47:42 +0100

    add crispy forms and pillow

--- chandra/settings.py
+++ chandra/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'main.apps.MainConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
