4dba9b90bf93a744831ee845b49f4cd44c6d82ea
wisie <wisnia501@gmail.com>
2023-01-18 23:29:40 +0100

    implement profile edit

--- main/models.py
+++ main/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateField()
-    content = models.TextField()
+    date_posted = models.DateField(auto_now_add=True)
+    content = models.TextField(max_length=280)
 
     def __str__(self) -> str:
         return f'Post by {self.author} on {self.date_posted} - {self.content[0:21]}'
