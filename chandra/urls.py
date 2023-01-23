42d35654b06069016e994f9404780bca411415ba
wisie <wisnia501@gmail.com>
2023-01-23 21:19:31 +0100

    implement working posting functionality

--- chandra/urls.py
+++ chandra/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from turtle import home
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -24,12 +25,17 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_views.index, name='index'),
+    path('', home_views.PostListView.as_view(), name='index'),
+    path('post/<pk>/', home_views.PostDetailView.as_view(), name='post-detail'),
+    path('post/create', home_views.PostCreateView.as_view(), name='post-create'),
+    path('post/<pk>/update/', home_views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<pk>/delete/', home_views.PostDeleteView.as_view(), name='post-delete'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('signup/', user_views.signup, name='signup'),
     path('logout/', user_views.logout_view, name='logout'),
     path('profile/', user_views.profile, name='profile-self'),
     path('settings/', home_views.settings_view, name='settings'),
+    # path('profile/<int:pk>', user_views.)
 ]
 
 if settings.DEBUG:
--- main/views.py
+++ main/views.py
@@ -1,11 +1,68 @@
 from django.shortcuts import render, redirect
 from users.forms import ProfileUpdateForm
-from users.models import Profile
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.views.generic import (
+    ListView,
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'main/index.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'main/index.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'main\components\post_create.html'
+    fields = ['content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def settings_view(request):
