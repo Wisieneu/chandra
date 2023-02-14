978cff2fbcafd702ee87ffc7e149d6c0d4c47d55
wisie <wisnia501@gmail.com>
2023-02-14 09:09:47 +0100

    initial infinite scrolling with pure JS

--- static/main/js/renderPost.js
+++ static/main/js/renderPost.js
@@ -50,6 +50,7 @@ function likeButton (post, isAuthenticated = isAuth) {
 
 
 function formatPost (post) {
+    console.log(post)
     const authorRef = (post.author.username == post.author.display_name) ? `${post.author.username}` : `${post.author.display_name} <span id='greyed-out-tag-name-span'>@${post.author.username}</span>`
     let formattedPost = `<div class="post-wrapper infinite-item" id="post-${post.id}" style="cursor: pointer;">
     <a class="PLACEHOLDER" href="profile/${post.author.username}"><img class="post-image" src="${post.author.pfp_url}"></a>
@@ -73,11 +74,10 @@ const responseType = 'json';
 xhr.responseType = responseType;
 xhr.open(method, url);
 xhr.onload = function () {
-    console.log(xhr.response)
     const serverResponse = xhr.response
     let listedItems = serverResponse.response
     let finalPostStr = ''
-    for (let i = 0; i<listedItems.length; i++) {
+    for (let i = 0; i<12; i++) {
         let post = listedItems[i]
         let currentItem = formatPost(post)
         finalPostStr += currentItem
@@ -86,3 +86,43 @@ xhr.onload = function () {
 }
 xhr.send()
 
+window.onscroll = function() {
+    if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight) {
+        alert('At the bottom!')
+        
+        const xhr = new XMLHttpRequest();
+        
+
+        xhr.responseType = 'json';
+        xhr.open('GET', 'json/posts/all');
+        xhr.onload = function () {
+            const nextPostDiv = document.getElementById("next-post")
+            const serverResponse = xhr.response
+            let listedItems = serverResponse.response
+            let finalPostStr = ''
+            let nextPostNumber = parseInt(nextPostDiv.innerHTML)
+
+            for (nextPostNumber; nextPostNumber<listedItems.length; nextPostNumber++) {
+                console.log(nextPostNumber, listedItems.length)
+                console.log(listedItems[nextPostNumber])
+                let post = listedItems[nextPostNumber]
+                if (post == undefined){
+                    end = document.getElementById('ending-tag')
+                    end.classList.remove('invisible')
+                    break
+                }
+                let currentItem = formatPost(post)
+                finalPostStr += currentItem
+            }
+            766                                         
+            if (nextPostNumber+1 == listedItems.length) {
+                end = document.getElementById('ending-tag')
+                end.classList.remove('invisible')
+            }
+            nextPostDiv.innerHTML = nextPostNumber += 9
+            postsDiv.innerHTML += finalPostStr
+
+        }
+        xhr.send()
+    }
+  }
