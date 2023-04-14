4f382226d816b9c0290091a18367f4817793da31
wisie <wisnia501@gmail.com>
2023-04-14 17:22:04 +0000

    add apache

--- chandra/settings.py
+++ chandra/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["46.41.148.67", "wizzie.pl", "127.0.0.1"]
 
