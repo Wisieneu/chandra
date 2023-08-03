64790b4b9310fa75ade9d8f622ba7524a7ab4817
wisie <wisnia501@gmail.com>
2023-08-03 04:03:27 +0200

    correct path to config file

--- chandra/settings.py
+++ chandra/settings.py
@@ -14,12 +14,11 @@ from pathlib import Path
 import os
 import json
 
-with open("/etc/chandra_config.json") as config_file:
-    config = json.load(config_file)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+with open(BASE_DIR+"chandra_config.json") as config_file:
+    config = json.load(config_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
